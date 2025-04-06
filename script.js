6ba9f27098b63ee2c447075129783de08014c3c4
swawiye <sovereign.wawiye@gmail.com>
2025-04-06 22:52:55 +0300

    functionality

--- script.js
+++ script.js
@@ -25,4 +25,45 @@ addBtn.addEventListener('click', function() {
         alert('Please slect a date');
         return;
     }
-})
+    expenses.push({category, amount, date});
+
+    totalAmount += amount;
+    totalAmountCell.textContent = totalAmount;
+
+    const newRow = expensesTableBody.insertRow();
+
+    const categoryCell = newRow.insertCell();
+    const amountCell = newRow.insertCell();
+    const dateCell = newRow.insertCell();
+    const deleteCell = newRow.insertCell();
+    const deleteBtn = document.createElement('button');
+
+    deleteBtn.textContent = 'Delete';
+    deleteBtn.classList.add('delete-btn');
+    deleteBtn.addEventListener('click', function() {
+        expenses.splice(expenses.indexOf(expense), 1);
+
+        totalAmount -= expense.amount;
+        totalAmountCell.textContent = totalAmount;
+
+        expensesTableBody.removeChild(newRow);
+    });
+
+    const expense = expenses[expenses.length - 1];
+    categoryCell.textContent = expense.category;
+    amountCell.textContent = expense.amount;
+    dateCell.textContent = expense.date;
+    deleteCell.appendChild(delete.Btn);
+});
+
+for (const expense of expenses) {
+    totalAmount += expense.amount;
+    totalAmountCell.textContent = totalAmount;
+
+    const categorySelect = document.getElementById('category-select');
+    const amountInput = document.getElementById('amount-input');
+    const dateInput = document.getElementById('date-input');
+    const btn = document.getElementById('add-btn');
+    const expensesTableBody = document.getElementById('expenses-table-body');
+    const totalAmountCell = document.getElementById('total-amount');
+}
