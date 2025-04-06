5f075ecff4763361b227d8c5052250824f829f48
swawiye <sovereign.wawiye@gmail.com>
2025-04-06 22:21:04 +0300

    functionality

--- script.js
+++ script.js
@@ -7,3 +7,22 @@ const dateInput = document.getElementById('date-input');
 const btn = document.getElementById('add-btn');
 const expensesTableBody = document.getElementById('expenses-table-body');
 const totalAmountCell = document.getElementById('total-amount');
+
+addBtn.addEventListener('click', function() {
+    const category = categorySelect.ariaValueMax;
+    const amount = Number(amountInput.value);
+    const date = dateInput.value;
+
+    if(category === '') {
+        alert('Please selct a category');
+        return;
+    }
+    if (isNaN(amount) || amount <=0) {
+        alert('Please enter a valid number');
+        return;
+    }
+    if (date === '') {
+        alert('Please slect a date');
+        return;
+    }
+})
