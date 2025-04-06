91af4e886b0b1931e72524989bcf1be10c334838
swawiye <sovereign.wawiye@gmail.com>
2025-04-06 22:15:12 +0300

    js functionality

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-let expenses = []
+let expenses = [];
+let totalAmount = 0;
+
+const categorySelect = document.getElementById('category-select');
+const amountInput = document.getElementById('amount-input');
+const dateInput = document.getElementById('date-input');
+const btn = document.getElementById('add-btn');
+const expensesTableBody = document.getElementById('expenses-table-body');
+const totalAmountCell = document.getElementById('total-amount');
